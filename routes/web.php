9a799e3dfc8a4ccf12fe68803b7518fdcbc1ba8a
ChristianIlliano <grafica.illiano@gmail.com>
2024-06-10 16:00:57 +0200

    feat list

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/movie', function () {
+    return view('Movie');
 });
+
+Route::get('/', [MovieController::class, 'index'])->name('home');
